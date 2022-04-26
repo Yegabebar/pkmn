6c9bffd384f210061991d6063660624bfd2cda8f
yegabebar <m.barbegayet@gmail.com>
2022-04-26 15:25:57 +0200

    Added local storage support

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import './App.css';
 import { PokeCard } from './components/PokeCard/PokeCard.js';
-import { pokemons } from './data/pokemons.js'
+import { pokemons } from './data/pokemons.js';
+
 function App() {
+  // Si pas d'objet pokemons en stockage, sauvegarder les pokemon en stockage local
+  if(!localStorage.getItem((pokemons))){
+    localStorage.setItem('pokemons', JSON.stringify(pokemons));
+  }
+  // puis utiliser les pokemons stockés en local pour les composants card
+  const storedPokemons = JSON.parse(localStorage.getItem('pokemons'));
   return (
     <div className="App">
-      {pokemons.map(pkmn => <PokeCard
+      {storedPokemons.map(pkmn => <PokeCard
         id={pkmn.id}
         name={pkmn.name}
         hp={pkmn.hp}
